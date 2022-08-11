1883ce280ccfbdedfae0b6bd94a9e6386f58c5a3
Vadim Constantin <vad.constantin@gmail.com>
2022-08-11 02:39:04 +0200

    airlines react components initialized

--- app/javascript/packs/components/Airlines/Airlines.jsx
+++ app/javascript/packs/components/Airlines/Airlines.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState, useEffetc, useEffect } from 'react'
+import AirlinesCard from './AirlinesCard'
+import './airlines.css'
 
 const Airlines = () => {
+
+  const [ airlines, setAirlines ] = useState([])
+  const url = "/api/v1/airlines.json"
+
+  useEffect(() => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      setAirlines(data.data)
+      console.log(data.data)})
+  }, [airlines.length])
+
+
   return (
-    <div>I am the Airlines #INDEX</div>
+    <div className="index-container">
+      <div>I am the Airlines #INDEX</div>
+      {airlines?.map((airline) => {
+        return (
+          <div key={airline.id}> <AirlinesCard airline={airline} /></div>)
+      })}
+    </div>
   )
 }
 
---
+++ app/javascript/packs/components/Airlines/AirlinesCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './airlines.css'
+
+const AirlinesCard = (props) => {
+  return(
+    <div>Airline Card number {props.airline.id}</div>
+  )
+}
+
+export default AirlinesCard
