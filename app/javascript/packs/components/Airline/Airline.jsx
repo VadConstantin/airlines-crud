043f8e6a19865b2cfa631af974fbc8fec13a380e
Vadim Constantin <vad.constantin@gmail.com>
2022-08-15 03:12:31 +0200

    added useNavigate

--- app/javascript/packs/components/Airline/Airline.jsx
+++ app/javascript/packs/components/Airline/Airline.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import './airline.css'
 
 
@@ -7,6 +7,7 @@ const url = "/api/v1/airlines.json"
 
 const Airline = () => {
 
+  const navigate = useNavigate()
   const params = useParams()
   const [airlines, setAirlines ] = useState([])
 
@@ -21,8 +22,13 @@ const Airline = () => {
   return(
     airlines.length > 0 && (
     <div className="show-page-container">
+      <button className="button-2" onClick={() => navigate(-1)}> Go back</button>
       <div className="show-card">
           <h1> {airline.attributes.name}</h1>
+          <img src={airline.attributes.image_url} className="airlines-show-card-img" />
+          <div className="average_score">
+            <p>Average score {airline.attributes.average_score}/5</p>
+          </div>
       </div>
     </div>
     )
